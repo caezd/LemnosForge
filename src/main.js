6bbf352fa6684c41a2f55646ced18b5c9d31e74e
Monomer <caedrikbl@gmail.com>
2022-02-12 16:36:27 -0500

    create lemnos md

--- src/main.js
+++
@@ -1,9 +0,0 @@
-// external exemple
-import $ from 'jquery';
-import foo from './foo.js';
-
-import Fetcher from "../../Helpers/Fetcher.js";
-
-export default function () {
-    console.log(Fetcher);
-};
